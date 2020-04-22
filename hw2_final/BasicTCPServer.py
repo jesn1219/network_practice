676f6b98e8befc41f9feaac16de25671e1b8921e
jesn1219 <jesn1219@gmail.com>
2020-04-22 16:46:05 +0900

    hw2 - Final

---
+++ hw2_final/BasicTCPServer.py
@@ -0,0 +1,111 @@
+# Submitter :
+# Jesoon Kang, 20170937
+# Last Modified : April 22, 2020
+# BasicTCPServer.py
+
+
+from socket import *
+import datetime
+
+
+# Class for translate App header & Data
+class AppPacket :
+    def __init__(self,data) :
+        data = str(data)
+        self.mode = int(data[0])
+        self.data = data[1:] 
+
+
+
+# Set TCP Server Port
+serverPort = 20937
+
+# Create TCP socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+print("The server is ready to receive on port", serverPort)
+
+
+# try catch for handling Ctrl+C Situation
+try :
+    # While Loop for Waiting new client
+    while (True) :
+        (connectionSocket, clientAddress) = serverSocket.accept()
+        print('Connection requested from', clientAddress)
+        socket_start_time = datetime.datetime.now()
+
+        while(True) :
+            
+            raw = connectionSocket.recv(2048)
+
+            # When clinet disconnected, it will get 0
+            if len(raw) == 0 :
+                break
+            
+            # mode 0~5
+            # 0 : Select Menu
+            # 1 ~ 5 : For Option 1~5
+            
+            # Data : String form 'Command number + data'
+            # Ex) Command 1, text : Hello World!
+            # It will send string : '1Hello World!'
+            # Translate raw data
+        
+            in_data = AppPacket(raw.decode())
+           
+            # For each command, Different serve
+            if in_data.mode == 1 :
+                print("Command ",in_data.mode)
+                mode = '1'
+                
+                # Get upper characters
+                data = in_data.data.upper()
+                raw = mode + data
+                connectionSocket.send(raw.encode())
+            
+            elif in_data.mode == 2 :
+                print("Command ",in_data.mode)
+                mode = '2'
+                # Get Client IP Address & Port
+                data = 'Reply from server: IP = {}, port = {}'.format(clientAddress[0],clientAddress[1])
+                raw = mode + data
+                connectionSocket.send(raw.encode())
+            elif in_data.mode == 3 :
+                print("Command ",in_data.mode)
+                mode = '3'
+                # Get Local server time
+                srv_time = datetime.datetime.now()
+                data = "time = {:02d}:{:02d}:{:02d}".format(srv_time.hour,srv_time.minute,srv_time.second)
+                raw = mode + data
+                connectionSocket.send(raw.encode())
+            elif in_data.mode == 4 :
+                print("Command ",in_data.mode)
+                mode = '4'
+                # Get Running time
+                run_time = datetime.datetime.now() - socket_start_time
+                run_time = run_time.seconds
+                hours, remainder = divmod(run_time, 3600)
+                minutes, seconds = divmod(remainder, 60)
+                data = "run time = {:02d}:{:02d}:{:02d}".format(hours,minutes,seconds)
+                raw = mode + data
+                connectionSocket.send(raw.encode())
+
+            elif in_data.mode == 5 :
+                # Socket For client is closed
+                print("Command ",in_data.mode)
+                connectionSocket.close()
+                print('Socket For Client is closed ')
+
+                # Server is waiting new client Now.
+                break
+
+
+except (KeyboardInterrupt) :
+    # For Ctrl + C on server
+    # Close Connection Socket
+    connectionSocket.close()
+
+# Close Server Socket
+serverSocket.close()
+print("\nBye bye~")
---
+++ hw2_final/BasicUDPClient.py
@@ -0,0 +1,175 @@
+# Submitter :
+# Jesoon Kang, 20170937
+# Last Modified : April 22, 2020
+# BasicUDPClient.py
+
+from socket import *
+import datetime
+
+
+# Class for translate App header & Data
+class AppPacket :
+    def __init__(self,data) :
+        data = str(data)
+        self.mode = int(data[0])
+        self.data = data[1:] 
+
+
+# Set UDP Server Address & Port
+serverName = 'nsl2.cau.ac.kr'
+serverPort = 30937
+
+# List for Menu Options 
+menu_options = \
+    ['convert text to UPPER-case',\
+     'get my IP address and port number',\
+     'get server time',\
+     'get server running time',\
+     'exit'\
+     ]
+
+# Function for display menu options
+def menu_present() :
+    print("\n<Menu>")
+    for i,opt in enumerate(menu_options) :
+        print(i+1,") ",opt)
+    print("")
+
+
+# Create Client UDP Socket 
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+# Set timeout
+clientSocket.settimeout(.02)
+print("The client is running on port", clientSocket.getsockname()[1])
+
+
+
+# try catch for handling Ctrl+C Situation
+try :
+    # mode 0~5
+    # 0 : Select Menu
+    # 1 ~ 5 : For Option 1~5
+  
+    # Data : String form 'Command number + data'
+    # Ex) Command 1, text : Hello World!
+    # It will send string : '1Hello World!'
+    mode = 0
+    while (True) :
+        if mode == 0 : 
+            # Present Menu Options
+            menu_present()
+
+            # Input Command
+            mode = input("Input option: ")
+           
+            # Check input option
+            try :
+                mode = int(mode) 
+            except (ValueError) :
+                None
+            if mode in range(1,len(menu_options)+1):
+                None
+            else :
+                print("Not available option")
+                mode = 0
+
+        elif mode == 1 :
+            # input text
+            data = input('Input lowercase sentence: ')
+            # Attach header
+            raw = str(mode) + data
+            
+            # Send to Server and Record Send time 
+            sent_time = datetime.datetime.now()
+            clientSocket.sendto(raw.encode(), (serverName, serverPort))
+            
+            # Receive data from Server
+            try : 
+                ret_raw = clientSocket.recv(2048)
+                received_time = datetime.datetime.now() 
+            except timeout :
+                print("Timed out : Connection Failed")
+                break
+            
+            # Unpack return data
+            ret_data = AppPacket(ret_raw.decode())
+            print("Reply from server : ",ret_data.data)
+            # Get RTT 
+            response_time = (received_time - sent_time).microseconds * 0.001
+            print("Response time : {:.2f} ms".format(response_time))
+            mode = 0
+        
+        elif mode == 2 :
+            raw = str(mode)
+            # Send to Server and Record Send time 
+            sent_time = datetime.datetime.now()
+            clientSocket.sendto(raw.encode(), (serverName, serverPort))
+            
+            # Receive data from Server
+            try : 
+                ret_raw = clientSocket.recv(2048)
+                received_time = datetime.datetime.now() 
+            except timeout :
+                print("Timed out : Connection Failed")
+                break
+            # Unpack return data
+            ret_data = AppPacket(ret_raw.decode())
+            print("Reply from server : ",ret_data.data)
+            # Get RTT 
+            response_time = (received_time - sent_time).microseconds * 0.001
+            print("Response time : {:.2f} ms".format(response_time))
+            mode = 0
+        elif mode == 3 :
+            raw = str(mode)
+            # Send to Server and Record Send time 
+            sent_time = datetime.datetime.now()
+            clientSocket.sendto(raw.encode(), (serverName, serverPort))
+            
+            # Receive data from Server
+            try : 
+                ret_raw = clientSocket.recv(2048)
+                received_time = datetime.datetime.now() 
+            except timeout :
+                print("Timed out : Connection Failed")
+                break
+            # Unpack return data
+            ret_data = AppPacket(ret_raw.decode())
+            print("Reply from server : ",ret_data.data)
+            # Get RTT 
+            response_time = (received_time - sent_time).microseconds * 0.001
+            print("Response time : {:.2f} ms".format(response_time))
+            mode = 0
+        elif mode == 4 :
+            raw = str(mode)
+            # Send to Server and Record Send time 
+            sent_time = datetime.datetime.now()
+            clientSocket.sendto(raw.encode(), (serverName, serverPort))
+            
+            # Receive data from Server
+            try : 
+                ret_raw = clientSocket.recv(2048)
+                received_time = datetime.datetime.now() 
+            except timeout :
+                print("Timed out : Connection Failed")
+                break
+            
+            # Unpack return data
+            ret_data = AppPacket(ret_raw.decode())
+            print("Reply from server : ",ret_data.data)
+            # Get RTT 
+            response_time = (received_time - sent_time).microseconds * 0.001
+            print("Response time : {:.2f} ms".format(response_time))
+            mode = 0
+
+        elif mode == 5 :
+            ret_raw = '5'
+            clientSocket.sendto(raw.encode(), (serverName, serverPort))
+            print("Terminate Connection")
+            break
+
+except (KeyboardInterrupt) :
+    None
+
+# Close Client Socket
+clientSocket.close()
+print('\nBye bye~')
